f95fb5bc52cd3964999f4af9e5a9347317132845
HYUNJIN LEE <dlguswls456@naver.com>
2024-07-18 17:39:00 +0900

    :tada: 모바일 프로젝트 초기 세팅
    안드로이드 프로젝트 초기 세팅

---
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/config/ApplicationClass.kt
@@ -0,0 +1,52 @@
+package com.circus.nativenavs.config
+
+import android.app.Application
+import com.circus.nativenavs.util.SharedPreferenceController
+
+class ApplicationClass : Application() {
+    companion object {
+        lateinit var sharedPreferences: SharedPreferenceController
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        sharedPreferences = SharedPreferenceController(applicationContext)
+
+//        initRetrofitInstance()
+    }
+
+//    // retrofit 인스턴스 생성, 레트로핏에 설정값 지정
+//    private fun initRetrofitInstance() {
+//        val client: OkHttpClient = OkHttpClient.Builder()
+//            .addInterceptor { chain ->
+//                val request = chain.request().newBuilder()
+//                    .addHeader("Authorization", "Bearer $GPT_KEY")
+//                    .build()
+//                chain.proceed(request)
+//            }
+//            .addInterceptor(HttpLoggingInterceptor().apply {
+//                level = HttpLoggingInterceptor.Level.BODY
+//            })
+//            .readTimeout(30, TimeUnit.SECONDS)
+//            .connectTimeout(30, TimeUnit.SECONDS)
+//            .build()
+//
+//        // retrofit 전역변수에 API url, 인터셉터, Gson 넣어주고 빌드
+//        GPTRetrofit = Retrofit.Builder()
+//            .baseUrl(GPT_BASE_URL)
+//            .client(client)
+//            .addConverterFactory(GsonConverterFactory.create(gson))
+//            .build()
+//
+//        weatherRetrofit = Retrofit.Builder()
+//            .baseUrl(WEATHER_BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create(gson))
+//            .build()
+//    }
+
+//    private val gson: Gson = GsonBuilder()
+//        .setLenient()
+//        .create()
+}
