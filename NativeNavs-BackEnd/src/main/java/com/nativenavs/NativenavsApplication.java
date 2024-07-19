d12b1562a49caa67fccaf7c7f9362b7ee7a936c0
최수환 <jerry5230@naver.com>
2024-07-19 16:47:34 +0900

    🎉 백엔드 프로젝트 초기 세팅
    .

---
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/NativenavsApplication.java
@@ -0,0 +1,13 @@
+package com.nativenavs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativenavsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativenavsApplication.class, args);
+	}
+
+}
