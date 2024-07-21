a9c91ac599200a5ae0652156d578d22a1f34736d
HYUNJIN LEE <dlguswls456@naver.com>
2024-07-21 20:56:56 +0900

    :sparkles: 로그인 UI 제작

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/login/LoginActivity.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/login/LoginActivity.kt
@@ -1,21 +1,18 @@
 package com.circus.nativenavs.ui.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.circus.nativenavs.R
+import com.circus.nativenavs.HomeActivity
+import com.circus.nativenavs.config.BaseActivity
+import com.circus.nativenavs.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
         }
+
     }
 }
