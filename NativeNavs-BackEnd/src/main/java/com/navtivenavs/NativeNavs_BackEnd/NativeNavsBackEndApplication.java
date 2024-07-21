9eb50be64e2c98cb5e5f62cfa21946d3642049e1
eoblue <eoblue22@naver.com>
2024-07-21 20:44:51 +0900

    ✨ 프로젝트 세팅 - WebMvcConfiguration 설정. Cors
    .

--- NativeNavs-BackEnd/src/main/java/com/navtivenavs/NativeNavs_BackEnd/NativeNavsBackEndApplication.java
+++ NativeNavs-BackEnd/src/main/java/com/navtivenavs/NativeNavs_BackEnd/NativeNavsBackEndApplication.java
@@ -2,8 +2,10 @@ package com.navtivenavs.NativeNavs_BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.nativenavs"})	// 디렉토리 생성 시 제대로 동작함.
 public class NativeNavsBackEndApplication {
 
 	public static void main(String[] args) {
