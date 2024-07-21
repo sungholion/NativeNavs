e97c70a61c3de8a508c8950d7e530731b5c2b5e5
DESKTOP-DD29VU1\MoonDongJin <mdj928@naver.com>
2024-07-22 00:48:09 +0900

    :sparkles: 리뷰 관련 프래그먼트 생성

---
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/review/ReviewRegisterFragment.kt
@@ -0,0 +1,16 @@
+package com.circus.nativenavs.ui.home.review
+
+import android.os.Bundle
+import android.view.View
+import com.circus.nativenavs.R
+import com.circus.nativenavs.config.BaseFragment
+import com.circus.nativenavs.databinding.FragmentReviewRegisterBinding
+
+class ReviewRegisterFragment : BaseFragment<FragmentReviewRegisterBinding>(FragmentReviewRegisterBinding::bind,R.layout.fragment_review_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
