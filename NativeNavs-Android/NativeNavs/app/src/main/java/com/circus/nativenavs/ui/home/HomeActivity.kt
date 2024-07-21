42fab9c355c019c7b7df84f2c760ae0866f0963a
DESKTOP-DD29VU1\MoonDongJin <mdj928@naver.com>
2024-07-22 00:48:24 +0900

    :sparkles: 투어 관련 프래그먼트 생성

---
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.circus.nativenavs.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.circus.nativenavs.R
+import com.circus.nativenavs.config.BaseActivity
+import com.circus.nativenavs.databinding.ActivityHomeBinding
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
