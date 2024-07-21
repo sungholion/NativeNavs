7f20ba53ca34fb98cd24fea2e9307642e94b56f6
DESKTOP-DD29VU1\MoonDongJin <mdj928@naver.com>
2024-07-22 00:47:40 +0900

    :sparkles: 예약 관련 프래그먼트 생성

---
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/reservation/ReservationDetailFragment.kt
@@ -0,0 +1,16 @@
+package com.circus.nativenavs.ui.home.reservation
+
+import android.os.Bundle
+import android.view.View
+import com.circus.nativenavs.R
+import com.circus.nativenavs.config.BaseFragment
+import com.circus.nativenavs.databinding.FragmentReservationDetailBinding
+
+class ReservationDetailFragment : BaseFragment<FragmentReservationDetailBinding>(FragmentReservationDetailBinding::bind,R.layout.fragment_reservation_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
