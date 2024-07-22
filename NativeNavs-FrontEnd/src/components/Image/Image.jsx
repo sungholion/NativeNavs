26c071e364813ffb2e7fbe0b57d1efa1c849f105
SnowySparks <originpc@naver.com>
2024-07-22 14:44:17 +0900

    ✨ 하트 이미지 컴포넌트 구현
    하트 이미지 컴포넌트 구현

--- NativeNavs-FrontEnd/src/components/Image/Image.jsx
+++ NativeNavs-FrontEnd/src/components/Image/Image.jsx
@@ -1,5 +1,17 @@
-const Image = () => {
-  return <div>Image</div>;
+import styles from "./Image.module.css";
+// url : 이미지링크 : string
+// alt : 설명 : string
+// size : 사이즈크기 : number
+// onClickEvent : 이미지 클릭시 발생하는 이벤트 : (?) => void
+const Image = ({ url = "", alt = "Empty", size = 1, onClickEvent }) => {
+  return (
+    <img
+      src={url}
+      alt={alt}
+      className={`${styles.img_default} ${styles[`img_${size}`]}`}
+      onClick={onClickEvent}
+    />
+  );
 };
 
 export default Image;
