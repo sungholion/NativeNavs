5616995701ef344d7521badd024f18814ecd45cf
mdj928 <moondj928@gmail.com>
2024-07-22 15:17:21 +0900

    :sparkles: NavBar Trav, Nav 별 페이지 연결

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/login/LoginActivity.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/login/LoginActivity.kt
@@ -2,9 +2,9 @@ package com.circus.nativenavs.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import com.circus.nativenavs.HomeActivity
 import com.circus.nativenavs.config.BaseActivity
 import com.circus.nativenavs.databinding.ActivityLoginBinding
+import com.circus.nativenavs.ui.home.HomeActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
