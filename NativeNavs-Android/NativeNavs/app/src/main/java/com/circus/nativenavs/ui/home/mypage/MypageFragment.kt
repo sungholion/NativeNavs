59f83740f19e97d87a35bb4a512478f767ea909b
mdj928 <moondj928@gmail.com>
2024-07-23 17:54:46 +0900

    :sparkles: 프로필 UI 개발, 리뷰 item_list, 프로필 카드 제작

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/mypage/MypageFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/mypage/MypageFragment.kt
@@ -5,12 +5,16 @@ import android.view.View
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
 import com.circus.nativenavs.databinding.FragmentMypageBinding
+import com.circus.nativenavs.util.navigate
 
 class MypageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::bind, R.layout.fragment_mypage) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.profile.setOnClickListener {
+            navigate(R.id.action_mypageFragment_to_profileFragment)
+        }
     }
 
 }
