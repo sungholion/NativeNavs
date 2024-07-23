6403d4f6d82b79b20ce3748e84ca5699457a9ccb
SnowySparks <originpc@naver.com>
2024-07-23 09:29:27 +0900

    🔥 App.jsx 코드 수정
    App.jsx 코드 개발에 불필요한 부분 전부 제거

--- NativeNavs-FrontEnd/src/App.jsx
+++ NativeNavs-FrontEnd/src/App.jsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import Button from "./components/Button/Button";
-import Heart from "./components/Heart/Heart";
-import Image from "./components/Image/Image";
-import Average from "./components/Star/Average";
-import Rating from "./components/Star/Rating";
-import CompletedTravelList from "./components/CompletedTourList/CompletedTravelList";
 
 function App() {
   return (
     <>
       <h1 className="test">Hello REACT</h1>
-      <div>dfafdf</div>
       <hr />
-
-      <Button />
-      <Heart />
-      <Image />
-      <Average />
-      <Rating />
-      <CompletedTravelList />
     </>
   );
 }
