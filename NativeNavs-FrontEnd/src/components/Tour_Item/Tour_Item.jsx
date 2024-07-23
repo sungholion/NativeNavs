354de49a382670aab79a19c16ca66c78a087d567
SnowySparks <originpc@naver.com>
2024-07-23 09:18:07 +0900

    🎨 Tour_Item 코드 수정
    Tour_Item 코드 수정

--- NativeNavs-FrontEnd/src/components/Tour_Item/Tour_Item.jsx
+++ NativeNavs-FrontEnd/src/components/Tour_Item/Tour_Item.jsx
@@ -2,33 +2,50 @@ import { tour } from "../../dummy";
 import styles from "./Tour_Item.module.css";
 import Rating from "../Star/Rating(Basic)";
 
-const Tour_Item = () => {
+// user_id : 유저 고유 키값
+// title : 투어 제목 :String
+// thumbnail_image : 투어 썸네일 이미지 : String
+// start_date : 시작날짜 (yyyy-mm-dd) : string
+// end_date : 끝 날짜 (yyyy-mm-dd) : string
+// nav_profile_img : 해당 가이드의 프로필 이미지 링크 : string
+// nav_nickname : 가이드 닉네임 : string
+// nav_language : 가이드가 사용가능한 언어 목록 : string[]
+const Tour_Item = ({
+  user_id,
+  title,
+  thumbnail_image,
+  start_date,
+  end_date,
+  review_average,
+  nav_profile_img,
+  nav_nickname,
+  nav_language,
+}) => {
   return (
     <div className={styles.tour_item}>
-      <img src={tour.img_url} alt="" className={styles.tour_thumnail} />
+      <img src={thumbnail_image} alt="" className={styles.tour_thumnail} />
       <section className={styles.tour_info}>
         <div className={styles.tour_leftinfo}>
-          <p className={styles.tour_title}>{tour.title}</p>
+          <p className={styles.tour_title}>{title}</p>
           <p className={styles.tour_duration}>
-            {tour.duration.start.toLocaleDateString()} ~{" "}
-            {tour.duration.end.toLocaleDateString()}
+            {start_date} ~ {end_date}
           </p>
-          <Rating avg={tour.avg} />
+          <Rating avg={review_average} />
         </div>
         <div className={styles.tour_rightinfo}>
           <div className={styles.tour_nav}>
-            <img src={tour.nav.Image} alt="" className={styles.nav_img} />
-            <p>{tour.nav.Nickname}</p>
+            <img src={nav_profile_img} alt="" className={styles.nav_img} />
+            <p>{nav_nickname}</p>
           </div>
           <div className={styles.tour_nav_language}>
             <img src="src/assets/language.png" alt="dd" />
-            {tour.nav.Languages.length > 1 ? (
+            {nav_language.length > 1 ? (
               <p>
-                {tour.nav.Languages[0]}외 {tour.nav.Languages.length - 1}
+                {nav_language[0]}외 {nav_language.length - 1}
                 개국어
               </p>
             ) : (
-              <p>{tour.nav.Languages[0]}</p>
+              <p>{nav_language[0]}</p>
             )}
           </div>
         </div>
