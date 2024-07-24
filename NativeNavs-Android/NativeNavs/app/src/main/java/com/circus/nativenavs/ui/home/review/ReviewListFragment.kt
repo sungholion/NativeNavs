15fe5f38fccc2e429a065b99a0a71b0ca95e126d
mdj928 <moondj928@gmail.com>
2024-07-24 16:32:18 +0900

    :sparkles: 리뷰, 리뷰 작성 UI 개발

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/review/ReviewListFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/review/ReviewListFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
 import com.circus.nativenavs.databinding.FragmentReviewListBinding
+import com.circus.nativenavs.util.CustomTitleWebView
+import com.circus.nativenavs.util.popBackStack
 
 
 class ReviewListFragment : BaseFragment<FragmentReviewListBinding>(FragmentReviewListBinding::bind, R.layout.fragment_review_list) {
@@ -12,6 +14,14 @@ class ReviewListFragment : BaseFragment<FragmentReviewListBinding>(FragmentRevie
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.reviewCustomWv.setOnBackListener(object : CustomTitleWebView.OnBackClickListener{
+            override fun onClick() {
+                popBackStack()
+            }
+
+        })
+
+        binding.reviewCustomWv.loadWebViewUrl("www.naver.com")
     }
 
 }
--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/review/ReviewRegisterFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/review/ReviewRegisterFragment.kt
@@ -5,12 +5,22 @@ import android.view.View
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
 import com.circus.nativenavs.databinding.FragmentReviewRegisterBinding
+import com.circus.nativenavs.util.CustomTitleWebView
+import com.circus.nativenavs.util.popBackStack
 
 class ReviewRegisterFragment : BaseFragment<FragmentReviewRegisterBinding>(FragmentReviewRegisterBinding::bind,R.layout.fragment_review_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.reviewRegisterCustomWv.apply {
+            setOnBackListener(object : CustomTitleWebView.OnBackClickListener{
+                override fun onClick() {
+                    popBackStack()
+                }
 
+            })
+            loadWebViewUrl("www.naver.com")
+        }
     }
 
 }
