0390fb88126238209bdba49e295164d582ca1aaf
HYUNJIN LEE <dlguswls456@naver.com>
2024-07-24 16:53:08 +0900

    :sparkles: 알림, 팀 소개, 약관 페이지 UI

---
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/tos/TosFragment.kt
@@ -0,0 +1,43 @@
+package com.circus.nativenavs.ui.tos
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import com.circus.nativenavs.R
+import com.circus.nativenavs.config.BaseFragment
+import com.circus.nativenavs.databinding.FragmentTosBinding
+import com.circus.nativenavs.ui.home.HomeActivity
+import com.circus.nativenavs.util.CustomTitleWebView
+import com.circus.nativenavs.util.popBackStack
+
+
+class TosFragment : BaseFragment<FragmentTosBinding>(FragmentTosBinding::bind,R.layout.fragment_tos) {
+
+    private lateinit var homeActivity: HomeActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        homeActivity = context as HomeActivity
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tosCustomWv.setOnBackListener(object : CustomTitleWebView.OnBackClickListener{
+            override fun onClick() {
+                popBackStack()
+            }
+
+        })
+
+        homeActivity.onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                if(!binding.tosCustomWv.backWebView()){
+                    popBackStack()
+                }
+            }
+
+        })
+    }
+}
