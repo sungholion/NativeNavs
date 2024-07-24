504d35e22ba8c3c20d347d0f0991a97a2b8b6bfc
조성호 <eoblue23@naver.com>
2024-07-24 17:52:04 +0900

    ✨ JWT를 적용한 로그인 - RefreshToken 갱신 완료
    .

---
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/auth/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.nativenavs.auth.config;
+
+import com.nativenavs.auth.jwt.JwtAuthenticationFilter;
+import com.nativenavs.auth.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+//                        .requestMatchers("/api/auth/login", "/api/auth/refresh").permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
--- NativeNavs-BackEnd/src/main/java/com/nativenavs/auth/controller/AuthController.java
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/auth/controller/AuthController.java
@@ -1,30 +1,31 @@
 package com.nativenavs.auth.controller;
 
+import com.nativenavs.auth.jwt.JwtTokenProvider;
 import com.nativenavs.auth.service.AuthService;
 import com.nativenavs.user.model.User;
-import com.nativenavs.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin("*") // 아직 고민..
+@CrossOrigin("*")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @Tag(name = "auth API", description = "Authentication")
     @Operation(summary = "이메일과 비밀번호로 로그인", description = "사용자의 이메일과 비밀번호로 로그인 합니다.")
@@ -41,14 +42,8 @@ public class AuthController {
                             )
                     )
             )
-            @RequestBody Map<String, String> credentials, HttpSession session){
-
+            @RequestBody Map<String, String> credentials) {
         Map<String, Object> response = new HashMap<>();
-
-        if(session.getAttribute("user") != null){
-            response.put("message", "이미 로그인된 유저입니다.");
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
-        }
 
         String email = credentials.get("email");
         String password = credentials.get("password");
@@ -57,8 +52,11 @@ public class AuthController {
         try {
             User user = authService.loginSessionWithEmail(email, password, device);
             if (user != null) {
-                session.setAttribute("user", user);
+                String accessToken = jwtTokenProvider.generateAccessToken(email);
+                String refreshToken = jwtTokenProvider.generateRefreshToken(email);
                 response.put("message", "로그인 성공");
+                response.put("accessToken", accessToken);
+                response.put("refreshToken", refreshToken);
                 return ResponseEntity.ok(response);
             } else {
                 response.put("message", "로그인 실패: 잘못된 이메일 또는 비밀번호");
@@ -71,19 +69,49 @@ public class AuthController {
         }
     }
 
-
     @Operation(summary = "로그아웃", description = "사용자를 로그아웃 합니다.")
     @ApiResponse(responseCode = "200", description = "로그아웃에 성공하였습니다.")
     @PostMapping("/logout")
-    public ResponseEntity<Map<String, Object>> logout(HttpSession session) {
-        Map<String, Object> response = authService.logout(session);
-
-        if ("로그아웃 성공".equals(response.get("message"))) {
-            return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
+    public ResponseEntity<Map<String, Object>> logout() {
+        // JWT는 서버에서 세션을 관리하지 않으므로, 클라이언트 측에서 토큰을 삭제하는 것으로 로그아웃을 대체합니다.
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "로그아웃 성공");
+        return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "리프레시 토큰을 통한 액세스 토큰 갱신", description = "리프레시 토큰을 사용하여 새로운 액세스 토큰을 생성합니다.")
+    @ApiResponse(responseCode = "200", description = "액세스 토큰 갱신 성공")
+    @PostMapping("/refresh")
+    public ResponseEntity<Map<String, Object>> refreshAccessToken(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "리프레시 토큰",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "{\"refreshToken\": \"your_refresh_token_here\"}"
+                            )
+                    )
+            )
+            @RequestBody Map<String, String> body) {
+        Map<String, Object> response = new HashMap<>();
+        String refreshToken = body.get("refreshToken");
+        try {
+            if (jwtTokenProvider.validateToken(refreshToken)) {
+                String email = jwtTokenProvider.getEmailFromToken(refreshToken);
+                String newAccessToken = jwtTokenProvider.generateAccessToken(email);
+                response.put("message", "액세스 토큰 갱신 성공");
+                response.put("accessToken", newAccessToken);
+                return ResponseEntity.ok(response);
+            } else {
+                response.put("message", "리프레시 토큰이 유효하지 않습니다.");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("message", "액세스 토큰 갱신 실패");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
 
 }
