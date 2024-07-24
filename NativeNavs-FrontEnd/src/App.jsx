5f24539a79be56d6c3aa2023f5d0b9ebb8c3f607
차상곤 <yg9618@naver.com>
2024-07-24 14:51:19 +0900

    ✨ 공통 컴포넌트 구현 - % 별점 표시 구현
    공통 컴포넌트 구현 - % 별점 표시 구현

--- NativeNavs-FrontEnd/src/App.jsx
+++ NativeNavs-FrontEnd/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import StarScore from "./components/Star/StarScore";
 
 function App() {
   return (
     <>
       <h1 className="test">Hello REACT</h1>
       <hr />
-      <StarScore score={80}/>
     </>
   );
 }
