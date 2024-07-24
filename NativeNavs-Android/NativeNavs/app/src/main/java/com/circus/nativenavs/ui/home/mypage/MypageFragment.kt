1bcf39c3bbc85b9dcb383079111e5d7df90e9aff
HYUNJIN LEE <dlguswls456@naver.com>
2024-07-24 15:32:01 +0900

    :sparkles: 마이페이지, 설정페이지 UI

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/mypage/MypageFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/home/mypage/MypageFragment.kt
@@ -5,12 +5,37 @@ import android.view.View
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
 import com.circus.nativenavs.databinding.FragmentMypageBinding
+import com.circus.nativenavs.util.navigate
 
-class MypageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::bind, R.layout.fragment_mypage) {
+class MypageFragment :
+    BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::bind, R.layout.fragment_mypage) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initEvent()
+    }
+
+    private fun initEvent() {
+        binding.mypageNotiIv.setOnClickListener {
+            navigate(R.id.action_mypageFragment_to_notificationFragment)
+        }
+
+        binding.mypageProfileCl.setOnClickListener {
+            navigate(R.id.action_mypageFragment_to_profileFragment)
+        }
+
+        binding.mypageSettingCl.setOnClickListener {
+            navigate(R.id.action_mypageFragment_to_appSettingFragment)
+        }
+
+        binding.mypageTeamCl.setOnClickListener {
+            navigate(R.id.action_mypageFragment_to_teamIntroFragment)
+        }
+
+        binding.mypageTosCl.setOnClickListener {
+            navigate(R.id.action_mypageFragment_to_tosFragment)
+        }
     }
 
 }
