689de076b9e5310dcea1519ad935ea3562e57b00
차상곤 <yg9618@naver.com>
2024-07-24 11:45:13 +0900

    🔥 App.jsx 복원
    app.jsx 복원

--- NativeNavs-FrontEnd/src/App.jsx
+++ NativeNavs-FrontEnd/src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import ScheduledTour from "./components/ScheduledTour/ScheduledTour.jsx";
-import Profile from "./components/Profile/Profile.jsx";
-import { tours } from "./dummy.jsx";
 
 function App() {
   return (
     <>
       <h1 className="test">Hello REACT</h1>
       <hr />
-      <ScheduledTour />
-
     </>
   );
 }
