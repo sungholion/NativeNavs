da7649c366799132aafe5884ed4baa9742ed451c
ansehdwls <mdj928@naver.com>
2024-07-28 23:24:18 +0900

    :sparkles: 프로필 정보 저장(국가, 앱언어 제외)

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/signup/SignUpProfileFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/signup/SignUpProfileFragment.kt
@@ -2,8 +2,10 @@ package com.circus.nativenavs.ui.signup
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
+import com.circus.nativenavs.data.signup.SignUpActivityViewModel
 import com.circus.nativenavs.databinding.FragmentSignUpProfileBinding
 import com.circus.nativenavs.util.navigate
 import com.circus.nativenavs.util.popBackStack
@@ -12,7 +14,7 @@ class SignUpProfileFragment : BaseFragment<FragmentSignUpProfileBinding>(
     FragmentSignUpProfileBinding::bind,
     R.layout.fragment_sign_up_profile
 ) {
-
+    private val signUpViewModel: SignUpActivityViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -25,6 +27,15 @@ class SignUpProfileFragment : BaseFragment<FragmentSignUpProfileBinding>(
         }
 
         binding.signupRegisterBtn.setOnClickListener {
+            val name = binding.signupNameEt.text.toString()
+            val nickname = binding.signupNicknameEt.text.toString()
+            val birth = binding.signupBirthEt.text.toString()
+            val phone = binding.signupPhoneEt.text.toString()
+            signUpViewModel.updateName(name)
+            signUpViewModel.updateNickname(nickname)
+            signUpViewModel.updateBirth(birth)
+            signUpViewModel.updatePhone(phone)
+            println(signUpViewModel.toString())
             navigate(R.id.action_signUpProfileFragment_to_signUpCompleteFragment)
         }
 
