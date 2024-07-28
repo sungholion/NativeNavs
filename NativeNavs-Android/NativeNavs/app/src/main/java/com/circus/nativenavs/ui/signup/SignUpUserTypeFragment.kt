ad40db281019256564120b453209f9a9b7a3962c
ansehdwls <mdj928@naver.com>
2024-07-28 23:24:36 +0900

    :sparkles: 유저 타입 저장

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/signup/SignUpUserTypeFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/signup/SignUpUserTypeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
+import com.circus.nativenavs.data.signup.SignUpActivityViewModel
 import com.circus.nativenavs.databinding.FragmentSignUpUserTypeBinding
 import com.circus.nativenavs.util.navigate
 import com.circus.nativenavs.util.popBackStack
@@ -15,6 +17,8 @@ class SignUpUserTypeFragment : BaseFragment<FragmentSignUpUserTypeBinding>(
     FragmentSignUpUserTypeBinding::bind,
     R.layout.fragment_sign_up_user_type
 ) {
+
+    private val signUpViewModel: SignUpActivityViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,10 +32,12 @@ class SignUpUserTypeFragment : BaseFragment<FragmentSignUpUserTypeBinding>(
         }
 
         binding.signupTypeNavLl.setOnClickListener {
+            signUpViewModel.updateIsNav(true)
             navigate(R.id.action_signUpUserTypeFragment_to_signUpProfileFragment)
         }
 
         binding.signupTypeTravLl.setOnClickListener {
+            signUpViewModel.updateIsNav(false)
             navigate(R.id.action_signUpUserTypeFragment_to_signUpProfileFragment)
         }
     }
