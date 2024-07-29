de163768a4df20c4c392a03e6f2332d074088142
차상곤 <yg9618@naver.com>
2024-07-29 18:00:18 +0900

    ✨ 위시리스트 페이지 구현 중
    위시리스트 페이지 구현 중

---
+++ NativeNavs-FrontEnd/src/page/WishList.jsx
@@ -0,0 +1,13 @@
+import styles from './WishList.module.css';
+import ScheduledTour from '../components/ScheduledTour/ScheduledTour';
+
+function WishList() {
+
+
+    return (
+        <div>
+        <ScheduledTour />dd</div>
+    )
+}
+
+export default WishList;
