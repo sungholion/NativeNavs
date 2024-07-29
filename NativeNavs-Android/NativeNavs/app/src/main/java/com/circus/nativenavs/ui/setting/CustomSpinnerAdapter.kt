c435f380af20c1049edb4215fbedd623ceaa458a
문동진 <mdj928@naver.com>
2024-07-29 12:58:51 +0900

    Revert "Merge branch 'Feat/MB/회원가입-화면-개발' into 'master'"
    This reverts merge request !3

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/setting/CustomSpinnerAdapter.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/setting/CustomSpinnerAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import com.circus.nativenavs.R
 import com.circus.nativenavs.databinding.ItemDropdownBinding
 import com.circus.nativenavs.databinding.ItemDropdownSelectedBinding
@@ -35,8 +34,6 @@ class CustomSpinnerAdapter(context: Context, list: ArrayList<String>) :
         } else {
             ItemDropdownBinding.bind(convertView)
         }
-
-        (parent as? ListView)?.overScrollMode = View.OVER_SCROLL_NEVER
 
         getItem(position)?.let {
             binding.itemTv.text = it
--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/signup/SignUpProfileFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/signup/SignUpProfileFragment.kt
@@ -1,13 +1,10 @@
 package com.circus.nativenavs.ui.signup
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
 import com.circus.nativenavs.R
 import com.circus.nativenavs.config.BaseFragment
 import com.circus.nativenavs.databinding.FragmentSignUpProfileBinding
-import com.circus.nativenavs.ui.setting.CustomSpinnerAdapter
 import com.circus.nativenavs.util.navigate
 import com.circus.nativenavs.util.popBackStack
 
@@ -16,37 +13,10 @@ class SignUpProfileFragment : BaseFragment<FragmentSignUpProfileBinding>(
     R.layout.fragment_sign_up_profile
 ) {
 
-    private lateinit var signUpActivity: SignUpActivity
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        signUpActivity = context as SignUpActivity
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initEvent()
-        initSpinner()
-    }
-
-    private fun initSpinner() {
-        val spinnerAdapter = CustomSpinnerAdapter(signUpActivity, SignUpLanguageFragment.COUNTRIES)
-        binding.signupNationalitySp.adapter = spinnerAdapter
-        binding.signupNationalitySp.setSelection(0)
-
-        val spinnerItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                spinnerAdapter.setSelectedItemPosition(position)
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                spinnerAdapter.setSelectedItemPosition(0)
-            }
-
-        }
-
-        binding.signupNationalitySp.onItemSelectedListener = spinnerItemSelectedListener
     }
 
     private fun initEvent() {
