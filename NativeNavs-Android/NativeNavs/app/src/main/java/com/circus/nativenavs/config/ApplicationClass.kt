b91133ad4dafe5bd54fc5d37374b44d116870855
mdj928 <moondj928@gmail.com>
2024-07-30 15:47:42 +0900

    :sparkles: 로그인 성공 시 SharedPref 저장

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/config/ApplicationClass.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/config/ApplicationClass.kt
@@ -2,57 +2,48 @@ package com.circus.nativenavs.config
 
 import android.app.Application
 import android.util.Patterns
-import com.circus.nativenavs.util.SharedPreferenceController
+import com.circus.nativenavs.util.AuthInterceptor
+import com.circus.nativenavs.util.NATIVENAVS_URL
 import java.util.regex.Pattern
 import com.circus.nativenavs.util.PREF
 import com.circus.nativenavs.util.SharedPref
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApplicationClass : Application() {
     companion object {
-        const val FRAGMENT_TOUR_LIST = 10
-        const val FRAGMENT_TOUR_DETAIL = 11
-        const val FRAGMENT_TOUR_REGISTER = 12
-
+        lateinit var retrofit: Retrofit
+        private val authInterceptor = AuthInterceptor("")
+        val gson: Gson = GsonBuilder().setLenient().create()
+        fun setAuthToken(token: String) {
+            authInterceptor.setAuthToken(token)
+        }
     }
 
     override fun onCreate() {
         super.onCreate()
 
         SharedPref.sharedPrefs = applicationContext.getSharedPreferences(PREF, MODE_PRIVATE)
-
-//        initRetrofitInstance()
+        initRetrofitInstance()
     }
 
-//    // retrofit 인스턴스 생성, 레트로핏에 설정값 지정
-//    private fun initRetrofitInstance() {
-//        val client: OkHttpClient = OkHttpClient.Builder()
-//            .addInterceptor { chain ->
-//                val request = chain.request().newBuilder()
-//                    .addHeader("Authorization", "Bearer $GPT_KEY")
-//                    .build()
-//                chain.proceed(request)
-//            }
-//            .addInterceptor(HttpLoggingInterceptor().apply {
-//                level = HttpLoggingInterceptor.Level.BODY
-//            })
-//            .readTimeout(30, TimeUnit.SECONDS)
-//            .connectTimeout(30, TimeUnit.SECONDS)
-//            .build()
-//
-//        // retrofit 전역변수에 API url, 인터셉터, Gson 넣어주고 빌드
-//        GPTRetrofit = Retrofit.Builder()
-//            .baseUrl(GPT_BASE_URL)
-//            .client(client)
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .build()
-//
-//        weatherRetrofit = Retrofit.Builder()
-//            .baseUrl(WEATHER_BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .build()
-//    }
+    private fun initRetrofitInstance() {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
 
-//    private val gson: Gson = GsonBuilder()
-//        .setLenient()
-//        .create()
+        retrofit = Retrofit.Builder()
+            .baseUrl(NATIVENAVS_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
 }
