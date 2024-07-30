eafaef3470b8b7724ed8fb4a35b89d821e1abe8d
mdj928 <moondj928@gmail.com>
2024-07-30 15:48:18 +0900

    :sparkles: 로그인 확인 여부

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/login/LoginActivity.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/login/LoginActivity.kt
@@ -2,22 +2,44 @@ package com.circus.nativenavs.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.circus.nativenavs.config.BaseActivity
+import com.circus.nativenavs.data.LoginDTO
 import com.circus.nativenavs.databinding.ActivityLoginBinding
 import com.circus.nativenavs.ui.home.HomeActivity
 import com.circus.nativenavs.ui.signup.SignUpActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+    private val activityViewModel: LoginActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initEvent()
 
+        activityViewModel.loginStatusCode.observe(this, Observer { statusCode ->
+            // 상태 코드 처리
+            if (statusCode == 200) {
+                showToast("로그인 성공")
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            } else {
+                showToast("로그인 실패")
+            }
+        })
+
+
     }
 
     private fun initEvent() {
         binding.loginBtn.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+//            val email = binding.loginEmailEt.text.toString()
+//            val password = binding.loginPwEt.text.toString()
+            val email = "eoblue23@gmail.com"
+            val password = "1234"
+            val device = ""
+
+            activityViewModel.Login(LoginDTO(email, password, device))
         }
 
         binding.loginSignupTv.setOnClickListener {
