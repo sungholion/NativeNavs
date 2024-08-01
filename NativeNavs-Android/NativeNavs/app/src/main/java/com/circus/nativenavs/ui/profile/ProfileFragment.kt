47adee5e79e0981839d7a03f2051a640112638b6
ansehdwls <mdj928@naver.com>
2024-08-01 21:24:01 +0900

    :sparkles: UserID로 검색한 프로필 데이터 연결

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/profile/ProfileFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/profile/ProfileFragment.kt
@@ -1,8 +1,12 @@
 package com.circus.nativenavs.ui.profile
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import androidx.core.net.toUri
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -12,6 +16,7 @@ import com.circus.nativenavs.data.ProfileReviewDto
 import com.circus.nativenavs.databinding.FragmentProfileBinding
 import com.circus.nativenavs.ui.home.HomeActivity
 import com.circus.nativenavs.ui.home.HomeActivityViewModel
+import com.circus.nativenavs.util.SharedPref
 import com.circus.nativenavs.util.navigate
 import com.circus.nativenavs.util.popBackStack
 
@@ -62,25 +67,49 @@ class ProfileFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initData()
         initView()
         initAdapter()
         initEvent()
 
 
     }
-    private fun initView(){
-        homeActivityViewModel.profileUser.observe(viewLifecycleOwner){
 
+    private fun initData() {
+        homeActivityViewModel.getProfileUser(SharedPref.userId ?: 10)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initView() {
+        homeActivityViewModel.profileUser.observe(viewLifecycleOwner) { it ->
+            if (SharedPref.userId != it.id) binding.profileModifyBtn.visibility = INVISIBLE
+            else binding.profileModifyBtn.visibility = VISIBLE
+
+            binding.profileUserIv.setImageURI(it.image.toUri())
+            binding.profileUserNameTv.text = it.name
+            binding.profileUserType.text =
+                if (it.isNav) getString(R.string.sign_type_nav) else getString(R.string.sign_type_trav)
+            binding.profileUserNation.apply {
+                text = getString(R.string.profile_nation) + " " + it.nation
+            }
+            binding.profileUserLanguage.apply {
+                text = getString(R.string.profile_user_language) + " " + it.userLanguage
+            }
+            binding.profileUserUseNum.apply {
+                text = getString(R.string.profile_user_use) + " " + it.travReservationCount
+            }
         }
     }
-    private fun initAdapter(){
+
+    private fun initAdapter() {
         binding.profileReviewRv.apply {
             this.adapter = ProfileReviewListAdapter().apply {
                 submitList(dummy)
             }
         }
     }
-    private fun initEvent(){
+
+    private fun initEvent() {
         binding.profileModifyBtn.setOnClickListener {
             navigate(R.id.action_profileFragment_to_profileModifylFragment)
         }
@@ -91,7 +120,8 @@ class ProfileFragment :
 
         binding.profileReviewTitle.setOnClickListener {
             //만약 nav이면
-            val action = ProfileFragmentDirections.actionProfileFragmentToReviewListFragment(navId = args.userId)
+            val action =
+                ProfileFragmentDirections.actionProfileFragmentToReviewListFragment(navId = args.userId)
             //만약 trav이면
 //            val action = ProfileFragmentDirections.actionProfileFragmentToReviewListFragment(travId = args.userId)
 
