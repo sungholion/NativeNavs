4960e32aca7430dc80665c077177fca3af5d62a5
SnowySparks <originpc@naver.com>
2024-08-02 13:51:57 +0900

    🐛 API 링크 수정
    API 링크 수정

--- NativeNavs-FrontEnd/src/page/TourCreate.jsx
+++ NativeNavs-FrontEnd/src/page/TourCreate.jsx
@@ -21,7 +21,7 @@ const TourCreate = () => {
     console.log(data);
     try {
       const response = await axios.post(
-        "https://i11d110.p.ssafy.io:8081/api/tours",
+        "https://i11d110.p.ssafy.io/api/tours",
         data,
         {
           headers: {
