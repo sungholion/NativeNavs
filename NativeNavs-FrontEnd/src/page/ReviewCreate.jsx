09c4cd3f5115b94f1c0a81d8f668c5af57619a51
SnowyPacks <originpc@naver.com>
2024-08-02 23:35:14 +0900

    ✨ 리뷰작성 구현완성
    사진 업로드, 별점부여, 후기글에 대한 것 완성

--- NativeNavs-FrontEnd/src/page/ReviewCreate.jsx
+++ NativeNavs-FrontEnd/src/page/ReviewCreate.jsx
@@ -50,14 +50,17 @@ const ReviewCreate = ({ info } = dummy_info) => {
         alert("최대 5개까지 업로드 가능합니다.");
         return;
       }
-
-      let fileArr = [...files];
+      const fileArr = [...files];
       for (let file of fileArr) {
         if (file.size > 1024 * 1024 * 10) {
           alert("10MB 이하의 파일만 업로드 가능합니다.");
           return;
         }
       }
+      for (let imgData of reviewData.image) {
+        URL.revokeObjectURL(imgData);
+      }
+      dispatch({ type: "image", image: fileArr });
     }
   };
 
@@ -75,12 +78,12 @@ const ReviewCreate = ({ info } = dummy_info) => {
       </section>
       <section className="ReviewImgUploadSection">
         <div className="ReviewImgUploadHeader">
-          사진 등록 :
-          <span>
-            {reviewData.image.length} / {MAX_IMAGE_COUNT}
-          </span>
-        </div>
-        <div className="ReviewImgUpload">
+          <div>
+            사진 등록 :
+            <span>
+              {reviewData.image.length} / {MAX_IMAGE_COUNT}
+            </span>
+          </div>
           <div>
             <label htmlFor="imgUploader">
               <div>+</div>
@@ -89,12 +92,49 @@ const ReviewCreate = ({ info } = dummy_info) => {
               className="ReviewImgUploader"
               type="file"
               accept="image/*"
+              multiple
               onChange={onImgChange}
               id="imgUploader"
               name="imgUploader"
             />
           </div>
         </div>
+        <div className="ReviewImgUpload">
+          {reviewData.image.length ? (
+            reviewData.image.map((img, idx) => (
+              <img key={idx} src={URL.createObjectURL(img)} alt="reviewImg" />
+            ))
+          ) : (
+            <div className="noImgUploaded ">아직 등록한 이미지가 없습니다.</div>
+          )}
+        </div>
+      </section>
+      <section className="ReviewContent">
+        <div>솔직한 후기를 남겨 주세요</div>
+        <textarea
+          placeholder="리뷰를 작성해주세요."
+          value={reviewData.content}
+          onChange={(e) =>
+            dispatch({ type: "content", content: e.target.value })
+          }
+        ></textarea>
+      </section>
+      <section className={`ReviewButtonSection`}>
+        <button
+          className={`ReviewCreateSend ${
+            reviewData.image.length === 0 || reviewData.content === ""
+              ? "notAbleButton"
+              : ""
+          }`}
+          disabled={
+            !(reviewData.image.length === 0 || reviewData.content !== "")
+          }
+          onClick={() => {
+            console.log("HI");
+          }}
+        >
+          제출
+        </button>
       </section>
     </div>
   );
