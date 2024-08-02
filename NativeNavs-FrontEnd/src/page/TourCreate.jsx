273ed895afa1037d9014bca34261c7a1512133b4
SnowySparks <originpc@naver.com>
2024-08-02 12:01:12 +0900

    ♻️ 투어작성코드 구조 개선 - 투어수정에 용이
    투어작성코드 구조 개선 - 투어수정에 용이

--- NativeNavs-FrontEnd/src/page/TourCreate.jsx
+++ NativeNavs-FrontEnd/src/page/TourCreate.jsx
@@ -3,19 +3,22 @@ import axios from "axios";
 const NotAllowImgData = true;
 const TourCreate = () => {
   const onCreate = async (data) => {
+    if (!data) {
+      console.log("데이터가 없어요!");
+    }
     if (NotAllowImgData) {
       data.thumbnailImage =
         "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png";
       data.plans = data.plans.map((plan) => {
         return {
           ...plan,
-          images: [
+          image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          ],
         };
       });
     }
-
+    data.userId = 10;
+    console.log(data);
     try {
       const response = await axios.post(
         "http://i11d110.p.ssafy.io:8081/api/tours",
@@ -28,11 +31,9 @@ const TourCreate = () => {
       );
       console.log(response);
       window.alert("성공했어요!");
-      return response;
     } catch (error) {
       console.error(error);
       window.alert("실패했어요 ㅠㅠ");
-      return error;
     }
   };
   return (
--- NativeNavs-FrontEnd/src/page/TourEdit.jsx
+++
@@ -1,7 +0,0 @@
-import TourEditorHead from "@/components/TourEditor/TourEditorHead";
-
-const TourEdit = () => {
-  return <div>TourEditor</div>;
-};
-
-export default TourEdit;
