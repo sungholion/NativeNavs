b9d340680684f709c9f64b57601847323c04d064
SnowySparks <originpc@naver.com>
2024-08-02 17:55:45 +0900

    ✨ 사진업로드 기능 부분적 추가
    사진업로드 기능 부분적 추가

--- NativeNavs-FrontEnd/src/page/ReviewCreate.jsx
+++ NativeNavs-FrontEnd/src/page/ReviewCreate.jsx
@@ -2,6 +2,8 @@ import "./ReviewCreate.css";
 import Tour_Item_mini_Review from "@/components/Tour_Item/Tour_Item_mini_Review";
 import { useEffect, useReducer, useState } from "react";
 import StarScoring from "@/components/Star/StarScoring";
+
+const MAX_IMAGE_COUNT = 5; // 최대 이미지 업로드 수
 const dummy_info = {
   tour: {
     // 투어 정보
@@ -28,6 +30,8 @@ const reducer = (state, action) => {
       return { ...state, score: action.score };
     case "content":
       return { ...state, content: action.content };
+    case "image":
+      return { ...state, image: action.image };
   }
 };
 
@@ -37,8 +41,25 @@ const ReviewCreate = ({ info } = dummy_info) => {
     description: "",
     image: [], //이미지 파일 - Max 5개
   });
-
   const [tour_info, setTour_info] = useState({});
+
+  const onImgChange = (e) => {
+    const { files } = e.target;
+    if (files && files.length > 0) {
+      if (files.length > MAX_IMAGE_COUNT) {
+        alert("최대 5개까지 업로드 가능합니다.");
+        return;
+      }
+
+      let fileArr = [...files];
+      for (let file of fileArr) {
+        if (file.size > 1024 * 1024 * 10) {
+          alert("10MB 이하의 파일만 업로드 가능합니다.");
+          return;
+        }
+      }
+    }
+  };
 
   return (
     <div className="ReviewCreate">
@@ -47,10 +68,33 @@ const ReviewCreate = ({ info } = dummy_info) => {
       </section>
 
       <section className="ScoreRating">
-        <h4>별점</h4>
+        <h4>Nav와 함께한 여행은 어땟나요?</h4>
         <StarScoring
           onRatingChange={(score) => dispatch({ type: "score", score })}
         />
+      </section>
+      <section className="ReviewImgUploadSection">
+        <div className="ReviewImgUploadHeader">
+          사진 등록 :
+          <span>
+            {reviewData.image.length} / {MAX_IMAGE_COUNT}
+          </span>
+        </div>
+        <div className="ReviewImgUpload">
+          <div>
+            <label htmlFor="imgUploader">
+              <div>+</div>
+            </label>
+            <input
+              className="ReviewImgUploader"
+              type="file"
+              accept="image/*"
+              onChange={onImgChange}
+              id="imgUploader"
+              name="imgUploader"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
