46465f321d523ff0549116c9e497999e7b7f9833
최수환 <jerry5230@naver.com>
2024-08-04 04:14:26 +0900

    ✨ 위시리스트 추가,제거 개발
    .

---
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/wishlist/repository/WishlistRepository.java
@@ -0,0 +1,15 @@
+package com.nativenavs.wishlist.repository;
+
+import com.nativenavs.wishlist.entity.WishlistEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+public interface WishlistRepository extends JpaRepository<WishlistEntity,Integer> {
+    // 특정 사용자의 모든 위시리스트 조회
+    List<WishlistEntity> findByUserId(int userId);
+
+    WishlistEntity findByUserIdAndTourId(int userId, int tourId);
+}
