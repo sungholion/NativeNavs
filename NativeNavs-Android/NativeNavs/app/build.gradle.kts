50386990b51d562f36c9fb7ef699a4180d36a355
ansehdwls <mdj928@naver.com>
2024-08-05 00:57:55 +0900

    :sparkles: 달력 기능 추가

--- NativeNavs-Android/NativeNavs/app/build.gradle.kts
+++ NativeNavs-Android/NativeNavs/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("kotlin-kapt")
     id("androidx.navigation.safeargs.kotlin")
+
 }
 
 android {
@@ -29,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         dataBinding = true
@@ -66,4 +67,8 @@ dependencies {
     // Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+//    implementation(libs.material.calendarview){
+//        exclude(group = "com.android.support", module = "support-compat")
+//    }
 }
