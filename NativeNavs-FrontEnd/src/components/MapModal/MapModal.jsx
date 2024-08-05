d3347186c02a16a1148b9be82409dfb9dc7b926f
SnowyPacks <originpc@naver.com>
2024-08-05 09:06:24 +0900

    ✨ 예약기능 큰틀 완성 및 지도모달
    예약기능 큰틀 완성 및 지도모달

--- NativeNavs-FrontEnd/src/components/MapModal/MapModal.jsx
+++ NativeNavs-FrontEnd/src/components/MapModal/MapModal.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MapModal.css";
 import { getStaticImage } from "@/utils/get-static-image";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+import axios from "axios";
+
+const defaultLoc = {
+  lat: 37.5642,
+  lng: 127.00169,
+};
 
 const MapModal = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [places, setPlaces] = useState([]);
+  const handleSearch = async () => {
+    if (!searchTerm) return;
+    try {
+      const response = await axios.get(
+        "https://maps.googleapis.com/maps/api/place/textsearch/json",
+        {
+          params: {
+            key: import.meta.env.VITE_GOOGLE_MAP_API,
+            fields: "formatted_address%2Cname",
+            input: "setSerachTerm",
+            language: "ko",
+            locationbias: `circle%3500%40${defaultLoc.lat}%2C${defaultLoc.lng}`,
+            inputtype: "textquery",
+          },
+        }
+      );
+      setPlaces(response.data.results);
+      console.log(response.data.results);
+    } catch (error) {
+      console.error("Error fetching places:", error);
+    }
+  };
   return (
     <div className="ModalBackground" onClick={(e) => e.preventDefault()}>
       <div className="mapModal">
@@ -12,17 +47,34 @@ const MapModal = () => {
         </section>
         <section className="SearchLocation">
           <h4>검색</h4>
-          <input type="text" />
+          <form action="">
+            <input
+              type="text"
+              placeholder="장소를 검색하세요"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={handleSearch}>검색</button>
+          </form>
         </section>
         <section className="MapSection">
           <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
             <Map
               style={{ width: "80vw", height: "20vh" }}
-              defaultCenter={{ lat: 37.5642, lng: 127.00169 }}
-              defaultZoom={15}
+              defaultCenter={defaultLoc}
+              defaultZoom={13}
               gestureHandling={"greedy"}
               disableDefaultUI={true}
-            />
+              mapId={import.meta.env.VITE_MAP_ID}
+            >
+              <AdvancedMarker
+                position={defaultLoc}
+                title={"AdvancedMarker with customized pin."}
+                onClick={() => {
+                  console.log("abcd");
+                }}
+              ></AdvancedMarker>
+            </Map>
           </APIProvider>
         </section>
         <section className="SearchResult"></section>
