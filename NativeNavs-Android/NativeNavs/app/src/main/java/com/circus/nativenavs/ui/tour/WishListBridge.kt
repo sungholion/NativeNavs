66627c64125d4d1ea4c3e4434eef728e9900b6bf
HYUNJIN LEE <dlguswls456@naver.com>
2024-08-05 23:32:21 +0900

    :sparkles: 위시리스트 sendUserData 기능 추가

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/tour/WishListBridge.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/tour/WishListBridge.kt
@@ -3,7 +3,9 @@ package com.circus.nativenavs.ui.tour
 import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import com.circus.nativenavs.data.UserDto
 import com.circus.nativenavs.ui.home.HomeActivity
+import com.google.gson.Gson
 
 private const val TAG = "WishListBridge"
 
@@ -23,5 +25,23 @@ class WishListBridge(
     fun navigateFromWishToTourListFragment() {
         fragment.navigateFromWishToTourListFragment()
         Log.d(TAG, "navigateFromWishToTourListFragment: ")
+    }
+
+    fun sendUserData(user: UserDto) {
+        val gson = Gson()
+        val json = gson.toJson(user)
+        val script = "javascript:getUserData('$json');"
+        Log.d(TAG, "sendUserData: $script")
+        evaluateWebViewFunction(script) { result ->
+            Log.d(TAG, "sendUserData: $result")
+        }
+
+    }
+
+    private fun evaluateWebViewFunction(
+        script: String,
+        callback: ((String) -> Unit)? = null,
+    ) {
+        return webView.evaluateJavascript(script, callback)
     }
 }
