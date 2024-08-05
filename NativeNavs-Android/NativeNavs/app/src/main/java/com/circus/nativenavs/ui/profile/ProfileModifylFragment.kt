bd04d45534e5118be169ac79c4c95cc40abef457
ansehdwls <mdj928@naver.com>
2024-08-06 01:48:18 +0900

    :bug: 수정 이후 navigation Bug 수정

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/profile/ProfileModifylFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/profile/ProfileModifylFragment.kt
@@ -18,14 +18,19 @@ import com.circus.nativenavs.data.SignUpDto
 import com.circus.nativenavs.databinding.FragmentProfileModifyBinding
 import com.circus.nativenavs.ui.home.HomeActivity
 import com.circus.nativenavs.ui.home.HomeActivityViewModel
+import com.circus.nativenavs.util.SharedPref
 import com.circus.nativenavs.util.isPasswordValid
 import com.circus.nativenavs.util.navigate
+import com.circus.nativenavs.util.popBackStack
 
-class ProfileModifylFragment : BaseFragment<FragmentProfileModifyBinding>(FragmentProfileModifyBinding::bind,R.layout.fragment_profile_modify) {
+class ProfileModifylFragment : BaseFragment<FragmentProfileModifyBinding>(
+    FragmentProfileModifyBinding::bind,
+    R.layout.fragment_profile_modify
+) {
 
     private lateinit var homeActivity: HomeActivity
 
-    private val homeActivityViewModel : HomeActivityViewModel by activityViewModels()
+    private val homeActivityViewModel: HomeActivityViewModel by activityViewModels()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -48,7 +53,7 @@ class ProfileModifylFragment : BaseFragment<FragmentProfileModifyBinding>(Fragme
         binding.profileModifyNicknameEt.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    homeActivityViewModel.updateNickNameCheck(false)
+                homeActivityViewModel.updateNickNameCheck(false)
             }
 
             override fun afterTextChanged(s: Editable?) {}
@@ -56,21 +61,27 @@ class ProfileModifylFragment : BaseFragment<FragmentProfileModifyBinding>(Fragme
 
     }
 
-    private fun initViewModelEvent(){
+    private fun initViewModelEvent() {
         homeActivityViewModel.apply {
-            updateStatus.observe(viewLifecycleOwner){ statusCode ->
-                when(statusCode){
-                    200 ->{
-                        showToast("업데이트 성공")
-                        navigate(R.id.action_profileModifylFragment_to_profileFragment)
-                    }
-                    else ->{
-                        showToast("업데이트 실패")
+            updateStatus.observe(viewLifecycleOwner) { statusCode ->
+                if(statusCode != -1){
+                    when (statusCode) {
+                        200 -> {
+                            showToast("업데이트 성공")
+                            updateUserNickName(binding.profileModifyNicknameEt.text.toString())
+                            updateUserPhone(binding.profileModifyPhoneEt.text.toString())
+                            updateStatusCode(-1)
+                            popBackStack()
+                        }
+
+                        else -> {
+                            showToast("업데이트 실패")
+                        }
                     }
                 }
 
             }
-            dupliState.observe(viewLifecycleOwner){ status ->
+            dupliState.observe(viewLifecycleOwner) { status ->
                 when (status.first) {
                     200 -> {
                         homeActivityViewModel.updateNickNameCheck(true)
@@ -80,27 +91,35 @@ class ProfileModifylFragment : BaseFragment<FragmentProfileModifyBinding>(Fragme
                     }
 
                     else -> {
-                        homeActivityViewModel.updateNickNameCheck(false)
-                        binding.profileModifyNicknameHelpTv.visibility = INVISIBLE
-                        binding.profileModifyDupliOk.visibility = INVISIBLE
-                        binding.profileModifyBad.visibility = VISIBLE
+                        if (binding.profileModifyNicknameEt.text.toString() ==
+                            homeActivityViewModel.userDto.value?.nickname
+                        )
+                            homeActivityViewModel.updateNickNameCheck(true)
+                        else {
+                            homeActivityViewModel.updateNickNameCheck(false)
+                            binding.profileModifyNicknameHelpTv.visibility = INVISIBLE
+                            binding.profileModifyDupliOk.visibility = INVISIBLE
+                            binding.profileModifyBad.visibility = VISIBLE
+                        }
                     }
                 }
             }
             languageList.observe(viewLifecycleOwner) { languageList ->
-                if(languageList != LanguageListDto(emptyList()))binding.profileModifySelectedLanguageTv.text = languageList.language.joinToString(", ")
+                if (languageList != LanguageListDto(emptyList())) binding.profileModifySelectedLanguageTv.text =
+                    languageList.language.joinToString(", ")
             }
         }
     }
-    fun initView(){
 
+    fun initView() {
 
+        homeActivityViewModel.updateNickNameCheck(true)
         binding.apply {
             homeActivityViewModel.profileUser.value?.let {
                 profileModifyNameEt.setText(it.name)
                 profileModifyNicknameEt.setText(it.nickname)
                 profileModifyNationalityEt.setText(it.nation)
-                profileModifyBirthEt.setText(it.birth.substring(0,9))
+                profileModifyBirthEt.setText(it.birth.substring(0, 9))
                 profileModifySelectedLanguageTv.setText(it.userLanguage)
                 profileModifyPhoneEt.setText(it.phone)
             }
@@ -108,36 +127,36 @@ class ProfileModifylFragment : BaseFragment<FragmentProfileModifyBinding>(Fragme
 
 
     }
-    fun initEvent(){
+
+    fun initEvent() {
         binding.profileModifyCompleteBtn.setOnClickListener {
 
             val password = binding.profileModifyPasswordEt.text.toString()
             val passwordCheck = binding.profileModifyPasswordCheckEt.text.toString()
-            if(!isPasswordValid(password)){
+            if (!isPasswordValid(password)) {
                 showToast(getString(R.string.profile_password_message))
-            }
-            else if(passwordCheck != password){
+            } else if (passwordCheck != password) {
                 showToast(getString(R.string.profile_password_check_message))
-            }
-            else if(homeActivityViewModel.nicknameCheck.value == false) showToast(getString(R.string.profile_nickname_check))
-            else{
+            } else if (homeActivityViewModel.nicknameCheck.value == false) showToast(getString(R.string.profile_nickname_check))
+            else {
                 homeActivityViewModel.let {
                     binding.apply {
-                            it.updateProfileModifyUser(
-                                SignUpDto(
-                                    email = it.userDto.value!!.email,
-                                    password = profileModifyPasswordEt.text.toString(),
-                                    isNav = it.userDto.value!!.isNav,
-                                    nickname = profileModifyNicknameEt.text.toString(),
-                                    userLanguage = profileModifySelectedLanguageTv.text.toString(),
-                                    name = it.userDto.value!!.name,
-                                    phone = profileModifyPhoneEt.text.toString(),
-                                    nation = it.userDto.value!!.nation,
-                                    birth = it.userDto.value!!.birth,
-                                    image = " ",
-                                    device = it.userDto.value!!.device,
-                                    isKorean = it.userDto.value!!.korean
-                        ))
+                        it.updateProfileModifyUser(
+                            SignUpDto(
+                                email = it.userDto.value!!.email,
+                                password = profileModifyPasswordEt.text.toString(),
+                                isNav = it.userDto.value!!.isNav,
+                                nickname = profileModifyNicknameEt.text.toString(),
+                                userLanguage = profileModifySelectedLanguageTv.text.toString(),
+                                name = it.userDto.value!!.name,
+                                phone = profileModifyPhoneEt.text.toString(),
+                                nation = it.userDto.value!!.nation,
+                                birth = it.userDto.value!!.birth,
+                                image = " ",
+                                device = it.userDto.value!!.device,
+                                isKorean = it.userDto.value!!.korean
+                            )
+                        )
                     }
                     it.updateUser()
                 }
