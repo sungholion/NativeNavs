d6275d0dcdc5928931f9347ec8912186fda79356
mdj928 <moondj928@gmail.com>
2024-08-07 14:40:08 +0900

    :white_check_mark: QR 테스트

--- NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/mypage/MypageFragment.kt
+++ NativeNavs-Android/NativeNavs/app/src/main/java/com/circus/nativenavs/ui/mypage/MypageFragment.kt
@@ -1,6 +1,5 @@
 package com.circus.nativenavs.ui.mypage
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -12,11 +11,13 @@ import com.circus.nativenavs.databinding.FragmentMypageBinding
 import com.circus.nativenavs.ui.home.HomeActivity
 import com.circus.nativenavs.ui.home.HomeActivityViewModel
 import com.circus.nativenavs.ui.login.LoginActivity
-import com.circus.nativenavs.ui.qr.QRScanActivity
+import com.circus.nativenavs.ui.qr.CustomCaptureActivity
+import com.circus.nativenavs.ui.qr.QRCreateActivity
 import com.circus.nativenavs.util.LOGOUT
 import com.circus.nativenavs.util.SharedPref
 import com.circus.nativenavs.util.navigate
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MypageFragment :
     BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::bind, R.layout.fragment_mypage) {
@@ -107,9 +108,34 @@ class MypageFragment :
             builder.show()
         }
         binding.mypageQrTestCl.setOnClickListener {
-            activity?.startActivity(Intent(requireContext(), QRScanActivity::class.java))
+            startQRCodeScan()
+        }
+        binding.mypageQrCreateCl.setOnClickListener {
+            startActivity(Intent(requireContext(),QRCreateActivity::class.java).apply {
+                action = "17"
+            })
+        }
+
+    }
+
+    private fun startQRCodeScan() {
+        IntentIntegrator.forSupportFragment(this).apply {
+            setOrientationLocked(true)
+            setPrompt("Scan a QR Code")
+            setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            setCaptureActivity(CustomCaptureActivity::class.java)
+            initiateScan()
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        intentResult?.contents?.let {
+            // Handle the scanned result here
+            showToast(it)
+        }
+    }
 
 }
