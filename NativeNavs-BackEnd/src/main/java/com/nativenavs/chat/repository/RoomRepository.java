1966b31cc3e9f3cb506003c75b0f26f3f05c3fb5
조성호 <eoblue23@naver.com>
2024-08-07 15:04:19 +0900

    🐛 Chat Branch Merge 오류 해결
    .

--- NativeNavs-BackEnd/src/main/java/com/nativenavs/chat/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nativenavs.chat.repository;
-
-import com.nativenavs.chat.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
