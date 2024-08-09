2be2864ed7c60bf1c89a9cfadd36e6f93e76959c
최수환 <jerry5230@naver.com>
2024-08-09 12:28:40 +0900

    🚑️ 플랜요청수정
    .

--- NativeNavs-BackEnd/src/main/java/com/nativenavs/tour/dto/PlanDTO.java
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/tour/dto/PlanDTO.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlanDTO {
+    private int id;
     private String field;         // 일정을 설명할 필드
     private String description;  // 일정 설명
     private String image;        // 일정 사진
