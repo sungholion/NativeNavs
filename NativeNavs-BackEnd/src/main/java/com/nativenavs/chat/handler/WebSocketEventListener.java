3842cc6b4b3596c6b7b3e398b01e6e366f709253
조성호 <eoblue23@naver.com>
2024-08-10 23:43:27 +0900

    🐛 RoomId header 수신 해결
    .

--- NativeNavs-BackEnd/src/main/java/com/nativenavs/chat/handler/WebSocketEventListener.java
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/chat/handler/WebSocketEventListener.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class WebSocketEventListener {
@@ -31,8 +33,10 @@ public class WebSocketEventListener {
     }
 
     private int getRoomIdFromSession(StompHeaderAccessor headerAccessor) {
-        // You need to implement logic to extract the room ID from the session or headers
-        // For example, if roomId is passed in headers:
-        return Integer.parseInt(headerAccessor.getNativeHeader("roomId").get(0));
+        List<String> roomIdHeader = headerAccessor.getNativeHeader("roomId");
+        if (roomIdHeader == null || roomIdHeader.isEmpty()) {
+            throw new IllegalArgumentException("Missing roomId header in WebSocket session");
+        }
+        return Integer.parseInt(roomIdHeader.get(0));
     }
 }
