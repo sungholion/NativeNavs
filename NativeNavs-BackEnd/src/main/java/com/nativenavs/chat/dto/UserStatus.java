b855e942c86f118ff8eea0edd644b9e1b70859dc
조성호 <eoblue23@naver.com>
2024-08-11 16:50:24 +0900

    ♻️ UserStatusDTO 추가
    .

---
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/chat/dto/UserStatus.java
@@ -0,0 +1,15 @@
+package com.nativenavs.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserStatus {
+    private boolean bothConnected;
+
+}
