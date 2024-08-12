677f7f470813717c379a71fcb17fb9585da399cf
조성호 <eoblue23@naver.com>
2024-08-12 13:21:18 +0900

    ♻️ 예약 메세지 수정
    .

--- NativeNavs-BackEnd/src/main/java/com/nativenavs/notification/service/FcmServiceImpl.java
+++ NativeNavs-BackEnd/src/main/java/com/nativenavs/notification/service/FcmServiceImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.nativenavs.notification.dto.FcmMessageDTO;
+import com.nativenavs.tour.service.TourService;
 import com.nativenavs.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -21,6 +22,8 @@ public class FcmServiceImpl implements FcmService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TourService tourService;
 
 //    @Override
 //    public int sendMessageTo(FcmSendDTO fcmSendDTO) throws IOException {
@@ -74,6 +77,8 @@ public class FcmServiceImpl implements FcmService {
         String sendTourId;
         String sendRoomId;
 
+        String tourTitle = tourService.findTourById(tourId).getTitle();
+
         // flag 1 : 채팅 / 2 : 예약 신청 완료 / 3 : 투어 종료 / 4 : 투어 예정 알림
         if(flag == 1){
             sendTitle = "채팅 시작";
@@ -84,10 +89,10 @@ public class FcmServiceImpl implements FcmService {
         }
         else if(flag == 2){
             sendTitle = "예약 신청 완료";
-            sendMessage = "부산 가이드 예약 신청이 완료되었습니다";
+            sendMessage = tourTitle + " 예약 신청이 완료되었습니다";
             sendReservationId = String.valueOf(reservationId);
-            sendTourId = String.valueOf(-1);
-            sendRoomId = String.valueOf(-1);
+            sendTourId = String.valueOf(tourId);
+            sendRoomId = String.valueOf(roomId);
         }
         else if(flag == 3){
             sendTitle = "투어 종료";
