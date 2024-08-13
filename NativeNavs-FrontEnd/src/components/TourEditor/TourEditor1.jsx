e5d855b5fc7bb07c0e221be409189764fab63110
SnowySparks <originpc@naver.com>
2024-08-13 16:30:45 +0900

    💄 조건에 안맞으면 UI 변경 추가
    조건에 안맞으면 UI 변경 추가

--- NativeNavs-FrontEnd/src/components/TourEditor/TourEditor1.jsx
+++ NativeNavs-FrontEnd/src/components/TourEditor/TourEditor1.jsx
@@ -7,6 +7,33 @@ import { categoryItemKr, categoryItemEng } from "@/utils/constant";
 import { TourDataContext, TourDispatchContext } from "./TourEditorHead";
 import { getImageUrl } from "@/utils/get-image-url";
 
+const checkData = (data) => {
+  let errorDate = 0;
+  if (data.thumbnailImage === "" || !data.thumbnailImage) {
+    errorDate |= 1 << 0;
+  }
+  if (data.title.trim() === "") {
+    errorDate |= 1 << 1;
+  }
+  if (!data.startDate || !data.endDate || data.startDate > data.endDate) {
+    errorDate |= 1 << 2;
+  }
+  if (data.maxParticipants < 1) {
+    errorDate |= 1 << 3;
+  }
+  if (data.price < 0) {
+    errorDate |= 1 << 4;
+  }
+  if (data.location.trim() === "") {
+    errorDate |= 1 << 5;
+  }
+  if (data.categoryIds.length === 0 || data.categoryIds.length > 4) {
+    errorDate |= 1 << 6;
+  }
+
+  return errorDate;
+};
+
 const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
   const {
     title,
@@ -18,6 +45,9 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
     maxParticipants,
     categoryIds,
   } = useContext(TourDataContext);
+
+  const [checkInput, setCheckInput] = useState(0); // 2진수로 각 자리 위치마다 문제점 표시
+  // 1: thumbnail, 2: title, 4: date, 8: maxPeople, 16: cost, 32: location, 64: theme
 
   const { onTourDataChange } = useContext(TourDispatchContext);
 
@@ -42,6 +72,7 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
         alert("10MB 이하의 이미지만 업로드 가능합니다.");
         return;
       }
+      setCheckInput((cur) => cur & ~(1 << 0));
       onTourDataChange("thumbnailImage", uploadFile);
       const reader = new FileReader();
       reader.readAsDataURL(uploadFile);
@@ -66,7 +97,9 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
   return (
     <section className={styles.TourEditor1}>
       <div className={styles.Thumbnail}>
-        <p>{user && user.isKorean ? "썸네일 사진" : "Thumbnail Photo"}</p>
+        <p style={{ color: `${checkInput & (1 << 0) ? "lightcoral" : ""}` }}>
+          {user && user.isKorean ? "썸네일 사진" : "Thumbnail Photo"}
+        </p>
         <label htmlFor="thumbnail">
           {prevThumbnailImage !== "" ? (
             <img src={prevThumbnailImage} alt="이미지 미리보기" />
@@ -97,15 +130,27 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
         </div>
       </div>
       <div className={styles.Title}>
-        <p>{user && user.isKorean ? "제목" : "Title"}</p>
+        <p style={{ color: `${checkInput & (1 << 1) ? "lightcoral" : ""}` }}>
+          {user && user.isKorean ? "제목" : "Title"}
+        </p>
         <input
           type="text"
           value={title}
-          onChange={(e) => onTourDataChange("title", e.target.value)}
+          onChange={(e) => {
+            onTourDataChange("title", e.target.value);
+            if (e.target.value.length > 0) {
+              setCheckInput((cur) => cur & ~(1 << 1));
+            } else {
+              setCheckInput((cur) => cur | (1 << 1));
+            }
+          }}
         />
       </div>
       <div className={styles.Date}>
-        <p className={styles.title}>
+        <p
+          className={styles.title}
+          style={{ color: `${checkInput & (1 << 2) ? "lightcoral" : ""}` }}
+        >
           {user && user.isKorean ? "기간" : "Duration"}
         </p>
         <div className={styles.DateSection}>
@@ -137,7 +182,9 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
         </div>
       </div>
       <div className={styles.MaxPeople}>
-        <p>{user && user.isKorean ? "최대 인원" : "Max Participants"}</p>
+        <p style={{ color: `${checkInput & (1 << 3) ? "lightcoral" : ""}` }}>
+          {user && user.isKorean ? "최대 인원" : "Max Participants"}
+        </p>
         <div className={styles.MaxPeopleInput}>
           <img
             src={getStaticImage("minus")}
@@ -153,7 +200,6 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
             src={getStaticImage("add")}
             alt=""
             onClick={() => {
-              console.log(maxParticipants);
               if (maxParticipants < 20) {
                 onTourDataChange("maxParticipants", maxParticipants + 1);
               }
@@ -162,7 +208,9 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
         </div>
       </div>
       <div className={styles.Cost}>
-        <p>{user && user.isKorean ? "예상 비용(₩)" : "Estimated Cost"}</p>
+        <p style={{ color: `${checkInput & (1 << 4) ? "lightcoral" : ""}` }}>
+          {user && user.isKorean ? "예상 비용(₩)" : "Estimated Cost"}
+        </p>
         <input
           type="number"
           value={price.toString()}
@@ -177,25 +225,45 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
             }
           }}
         />
-        <div style={{ marginLeft: "26px", color: "#595959" }}>
+        <div style={{ marginLeft: "26px", color: "#595959", fontSize: "15px" }}>
           {user?.isKorean
             ? "최대 1,000,000₩ 까지 설정 가능합니다"
             : "You can set up to a maximum of 1,000,000 KRW"}
         </div>
       </div>
-      <div className={styles.locationSection}>
-        <p>{user?.isKorean ? "장소" : "Location"}</p>
+      <div
+        className={styles.locationSection}
+        style={{ color: `${checkInput & (1 << 5) ? "lightcoral" : ""}` }}
+      >
+        <p>
+          {user?.isKorean ? "장소" : "Location"}
+          <span
+            style={{ color: "#595959", fontSize: "14px", marginLeft: "10px" }}
+          >
+            {location.length}/{100}
+          </span>
+        </p>
         <input
           type="text"
           onChange={(e) => {
-            if (e.target.value.length <= 100) {
+            if (e.target.value.length === 0) {
               onTourDataChange("location", e.target.value);
+              setCheckInput((cur) => cur | (1 << 5));
+            } else if (e.target.value.length <= 100) {
+              onTourDataChange("location", e.target.value);
+              setCheckInput((cur) => cur & ~(1 << 5));
             }
           }}
         />
       </div>
       <div className={styles.Theme}>
-        <p>{user && user.isKorean ? "테마" : "Theme"}</p>
+        <p style={{ color: `${checkInput & (1 << 6) ? "lightcoral" : ""}` }}>
+          {user && user.isKorean ? "테마" : "Theme"}
+          <span>
+            {" "}
+            {categoryIds.length} / {4}
+          </span>
+        </p>
         <div className={styles.themeList}>
           {chunkedCategoryItems.map((chunk, index) => (
             <div key={index} className={styles.themeRow}>
@@ -209,11 +277,21 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
                   }`}
                   onClick={() => {
                     if (categoryIds.includes(item.idx)) {
+                      if (categoryIds.length === 0) {
+                        setCheckInput((cur) => cur | (1 << 6));
+                      } else if (categoryIds.length === 5) {
+                        setCheckInput((cur) => cur & ~(1 << 6));
+                      }
                       onTourDataChange(
                         "categoryIds",
                         categoryIds.filter((id) => id !== item.idx)
                       );
                     } else {
+                      if (categoryIds.length <= 3) {
+                        setCheckInput((cur) => cur & ~(1 << 6));
+                      } else {
+                        setCheckInput((cur) => cur | (1 << 6));
+                      }
                       onTourDataChange("categoryIds", [
                         ...categoryIds,
                         item.idx,
@@ -234,7 +312,28 @@ const TourEditor1 = ({ BeforePage, goAfterPage, user }) => {
           className={styles.rightButton}
           onClick={() => {
             onTourDataChange("themes");
-            goAfterPage();
+            let res = checkData({
+              thumbnailImage,
+              title,
+              startDate,
+              endDate,
+              maxParticipants,
+              price,
+              location,
+              categoryIds,
+            });
+            if (res === 0) {
+              goAfterPage();
+            } else {
+              alert(
+                `${
+                  user?.isKorean
+                    ? "항목들을 확인해주세요"
+                    : "Please check field(s)"
+                }`
+              );
+            }
+            setCheckInput(res);
           }}
         >
           {user && user.isKorean ? "다음" : "Next"}
